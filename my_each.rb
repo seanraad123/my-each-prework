7ae7ca411899af844b904af88417c0ea111025af
Sean Conrad <seanconrad123@gmail.com>
2018-06-16 18:31:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i]) if block_given?
+    i += 1
+  end
+  array
 end
+
+cars = ["car", "bike", "boat", "plane"]
+
+my_each(cars)
